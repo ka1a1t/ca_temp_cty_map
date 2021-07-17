b323c9111ef2a7b26ea3d8294097aa7411f03348
blah <blah@t440p>
2021-07-17 13:35:36 -0700

    App and web scraper added

---
+++ map_app/main.py
@@ -0,0 +1,207 @@
+# Import libraries
+import pandas as pd
+import numpy as np
+import math
+import os
+
+import geopandas
+import json
+
+from calendar import month_abbr
+
+from bokeh.io import output_notebook, show, output_file
+from bokeh.plotting import figure
+from bokeh.models import GeoJSONDataSource, LinearColorMapper, ColorBar, NumeralTickFormatter
+from bokeh.palettes import brewer
+
+from bokeh.io.doc import curdoc
+from bokeh.models import Slider, HoverTool, Select
+from bokeh.layouts import row, column, Column
+from bokeh.themes import built_in_themes, Theme
+
+cwd = os.getcwd()
+print(cwd)
+
+data_stem_pth = os.path.join(os.getcwd(), 'map_app', 'data')
+
+theme_pth = os.path.join(os.getcwd(), 'map_app', 'templates', 'theme.yml')
+
+# load weather data
+sta_wthr_pth = os.path.join(data_stem_pth, '2016_2020_ca_tmax_tmin_mnth.csv')
+sta_wthr = pd.read_csv(sta_wthr_pth)
+
+# sanity check should have 58 counties
+print(len(sta_wthr.county.unique()))
+
+# load geojson files
+geojson_pth = os.path.join(data_stem_pth, 'CA_Counties_TIGER2016.geojson')
+ca = geopandas.read_file(geojson_pth)
+
+
+
+# define cooredinate reference system for data projection
+# ESPG code 4326 is equiv to WGS84 lat-long projection
+ca.crs = {'init': 'epsg:4326'}
+
+ca = ca.rename(columns={'NAME':'county'}) 
+
+# check to see if names are identical: True
+# print("Are the county names of ca geopandas identical to weather station data: ",
+#       set(ca.county) == set(sta_wthr.county.unique()))
+
+
+
+# function to return json_data for a user selected month and year
+def json_wthr_data(selec_yr, selec_mnth):
+    yr = selec_yr
+    mnth = selec_mnth
+    selec_df = sta_wthr.copy()
+
+    # rip selected month and year from the weather station data
+    selec_df = selec_df[selec_df['month'] == mnth]
+    selec_df = selec_df[selec_df['year'] == yr]
+
+
+    # combine ca (GeoDataframe) with filtered weather station data
+    df_mrgd = pd.merge(ca, selec_df, on ='county', how='left')
+
+    # NA values to be filled
+    vals = {'year': yr, 'month': mnth}
+    df_mrgd = df_mrgd.fillna(value=vals)
+
+    # convert to df to json
+    json_mrgd = json.loads(df_mrgd.to_json())
+
+    # convert to json string-like object
+    json_data = json.dumps(json_mrgd)
+
+
+    return json_data
+
+
+################################################################################
+################################################################################
+################# Interactive Map ##############################################
+################################################################################
+################################################################################
+
+# df to help with selecting either max or min temp data
+# during plot creation and update callbacks
+plt_scheme_data = [('TMAX', 0, 130, '0,0', 'Average Maximum Temperature'),
+               ('TMIN', 0, 130, '0,0', 'Average Minimum Temperature')]
+
+
+plt_scheme_df = pd.DataFrame(plt_scheme_data, columns=['field', 'min_range', 'max_range',
+                                               'format', 'text'])
+
+# name_of_mnth: number_of_mnth
+mnth_dict = {b:a for a, b in enumerate(month_abbr[1::], start=1)}
+
+
+# map callback function: update_plot
+def update_plot(attr, old, new):
+    # the input yr is the year selected from the slider_yr
+    yr = slider_yr.value
+    # mnth = slider_mnth.value
+    mnth_txt = select_mnth.value
+    mnth = mnth_dict[mnth_txt]
+    
+    new_data = json_wthr_data(yr, mnth)
+
+    # the input tmp_dtype is either TMAX or TMIN input from select_temp box
+    tmp_dtype = select_temp.value
+    input_field = plt_scheme_df.loc[plt_scheme_df['text'] == tmp_dtype, 'field'].iloc[0]
+
+    # update plot with new inputs
+    p = make_plot(input_field)
+
+    # update layout, clear old document, create new document
+    layout = column(p, Column(select_temp), Column(slider_yr), Column(select_mnth))
+    curdoc().clear()
+    curdoc().add_root(layout)
+
+    # new data for updated plot
+    geosource.geojson = new_data
+
+
+# create a plotting function
+def make_plot(field_name):
+    # define colorbar
+    min_range = plt_scheme_df.loc[plt_scheme_df['field'] == field_name,
+                                  'min_range'].iloc[0]
+    max_range = plt_scheme_df.loc[plt_scheme_df['field'] == field_name, 'max_range'].iloc[0]
+    field_format = plt_scheme_df.loc[plt_scheme_df['field'] == field_name, 'format'].iloc[0]
+
+    # LinearColorMapper: num_range --> color sequence
+    color_mapper = LinearColorMapper(palette=palette, low=min_range, high=max_range)
+
+
+    # colorbar to help illustrate temperature
+    format_tick = NumeralTickFormatter(format=field_format)
+    color_bar = ColorBar(color_mapper=color_mapper, label_standoff=18,
+                         formatter=format_tick, border_line_color=None,
+                         major_label_text_color='#fefefe',
+                         location=(0, 0))
+
+    # figure object
+    text = plt_scheme_df.loc[plt_scheme_df['field'] == field_name, 'text'].iloc[0]
+
+    p = figure(title = text + ' by California County - 2016 to 2020 (°F)',
+               plot_height=650, plot_width=850,
+               toolbar_location=None)
+
+    p.xgrid.grid_line_color = None
+    p.ygrid.grid_line_color = None
+    p.axis.visible = False
+
+    # render patch to figure: render map of California onto figure
+    p.patches('xs', 'ys', source=geosource,
+              fill_color={'field': field_name, 'transform': color_mapper},
+              line_color='black', line_width=0.25, fill_alpha=1)
+
+    # specify color bar layout
+    p.add_layout(color_bar, 'right')
+
+    # add the hover tool to the graph
+    p.add_tools(hover)
+    return p
+
+
+# initial year 2020, month Jan, and weather data datatype TMAX
+geosource = GeoJSONDataSource(geojson = json_wthr_data(2020, 1))
+input_field = 'TMAX'
+
+
+# color palette definition
+palette = brewer['RdYlBu'][10]
+
+
+# create hover tool
+hover = HoverTool(tooltips = [ ('County', '@county'),
+                               ('Maximum Temperature', '@TMAX'),
+                               ('Minimum Temperature', '@TMIN')])
+
+# Call plotting function to generate figure
+p = make_plot(input_field)
+
+# slider_yr: Slider object to select views based on year
+slider_yr = Slider(title='Year', start=2016, end=2020, step=1, value = 2020)
+slider_yr.on_change('value', update_plot)
+
+# select_temp: Select object to select views based on TMAX or TMIN
+select_temp = Select(title = 'Temp Max/Min', value='Average Maximum Temperature',
+                options=['Average Maximum Temperature', 'Average Minimum Temperature'])
+select_temp.on_change('value', update_plot)
+
+
+# select_mnth: Select object to select views based on month
+select_mnth = Select(title="Month", value='Jan', options=list(month_abbr[1::]))
+select_mnth.on_change('value', update_plot)
+
+
+# make a column layout of Column(widget_name) and plot, and add it to the current document
+# display the current document
+layout = column(p, Column(select_temp), Column(slider_yr), Column(select_mnth))
+
+curdoc().theme = Theme(filename=theme_pth)
+curdoc().add_root(layout)
---
+++ web_scrape/wthr_filter.py
@@ -0,0 +1,64 @@
+#! /usr/bin/env python 
+
+
+# Import libraries
+import pandas as pd
+import numpy as np
+import math
+
+
+
+# convert C to F rounded to 2 decimals
+def cel_to_fahr(c):
+    f = (c * (9/5)) + 32
+    return round(f, 2)
+
+
+# function to load weather data
+def wthr_csv_to_df(wthr_csv):
+    ###import, clean, and sort data
+    sta_pts = pd.read_csv("./temp_stations.csv")
+    wthr_pts = pd.read_csv(wthr_csv)
+
+    # subset only temperature data
+    wthr_pts = wthr_pts[wthr_pts.wthr_dtype.isin(['TMAX', 'TMIN'])]
+
+    # merge station and weather df's
+    sta_wthr = pd.merge(sta_pts, wthr_pts, on='station_id')
+
+    # create a column of datetime entries
+    sta_wthr['DateTime'] = sta_wthr['date'].apply(lambda x: pd.to_datetime(str(x), format='%Y%m%d'))
+    sta_wthr['month'] = sta_wthr['DateTime'].dt.month
+    sta_wthr['year']  = sta_wthr['DateTime'].dt.year
+
+    # compute mean wthr value in each county for a given year month
+    # and wthr_dtype  (TMIN, TMAX)
+    wthr_grp_df = (sta_wthr.groupby(['county', 'year', 'month', 'wthr_dtype'])
+                           .wthr_val.mean()
+                           .reset_index())
+
+    # convert wthr_val from tenths of Celsius to Fahrenheit
+    wthr_grp_df['wthr_val'] = wthr_grp_df['wthr_val'].div(10).apply(cel_to_fahr)
+
+    # convert to wide format using pivot
+    wthr_grp_df = wthr_grp_df.pivot(index=['county', 'year', 'month'],
+                                    columns='wthr_dtype', values='wthr_val')
+
+    wthr_grp_df = wthr_grp_df.reset_index().rename_axis(None, axis=1)
+
+    
+    return wthr_grp_df
+
+
+if __name__ == "__main__":
+    data_name = "2016_2020_ca_wthr.csv"
+    sta_wthr = wthr_csv_to_df("./" + data_name)
+    print("Filtering by month:", data_name)
+    print("Done!")
+    # print(sta_wthr.head())
+    
+
+    # sanity check should have 58 counties
+    print("Number of unique counties present in data: ",
+          len(sta_wthr.county.unique()))
+    sta_wthr.to_csv('./2016_2020_ca_tmax_tmin_mnth.csv', index=False)
