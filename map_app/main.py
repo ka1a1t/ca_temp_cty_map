18b0118126a6f0a794b2a7be958dd3167e74c6cc
blah <blah@t440p>
2023-05-21 12:52:05 -0700

    update map_app and requirements.txt for bokeh 3.1

--- map_app/main.py
+++ map_app/main.py
@@ -1,4 +1,3 @@
-# Import libraries
 import pandas as pd
 import numpy as np
 import math
@@ -9,18 +8,14 @@ import json
 
 from calendar import month_abbr
 
-from bokeh.io import output_notebook, show, output_file
 from bokeh.plotting import figure
 from bokeh.models import GeoJSONDataSource, LinearColorMapper, ColorBar, NumeralTickFormatter
 from bokeh.palettes import brewer
 
 from bokeh.io.doc import curdoc
 from bokeh.models import Slider, HoverTool, Select
-from bokeh.layouts import row, column, Column
+from bokeh.layouts import column, Column
 from bokeh.themes import built_in_themes, Theme
-
-cwd = os.getcwd()
-print(cwd)
 
 data_stem_pth = os.path.join(os.getcwd(), 'map_app', 'data')
 
@@ -31,17 +26,15 @@ sta_wthr_pth = os.path.join(data_stem_pth, '2016_2020_ca_tmax_tmin_mnth.csv')
 sta_wthr = pd.read_csv(sta_wthr_pth)
 
 # sanity check should have 58 counties
-print(len(sta_wthr.county.unique()))
+# print(len(sta_wthr.county.unique()))
 
 # load geojson files
 geojson_pth = os.path.join(data_stem_pth, 'CA_Counties_TIGER2016.geojson')
 ca = geopandas.read_file(geojson_pth)
 
-
-
 # define cooredinate reference system for data projection
 # ESPG code 4326 is equiv to WGS84 lat-long projection
-ca.crs = {'init': 'epsg:4326'}
+ca.crs = 'epsg:4326' 
 
 ca = ca.rename(columns={'NAME':'county'}) 
 
@@ -50,17 +43,13 @@ ca = ca.rename(columns={'NAME':'county'})
 #       set(ca.county) == set(sta_wthr.county.unique()))
 
 
-
 # function to return json_data for a user selected month and year
 def json_wthr_data(selec_yr, selec_mnth):
     yr = selec_yr
     mnth = selec_mnth
-    selec_df = sta_wthr.copy()
 
     # rip selected month and year from the weather station data
-    selec_df = selec_df[selec_df['month'] == mnth]
-    selec_df = selec_df[selec_df['year'] == yr]
-
+    selec_df = sta_wthr[(sta_wthr['month'] == mnth) & (sta_wthr['year'] == yr)]
 
     # combine ca (GeoDataframe) with filtered weather station data
     df_mrgd = pd.merge(ca, selec_df, on ='county', how='left')
@@ -75,21 +64,17 @@ def json_wthr_data(selec_yr, selec_mnth):
     # convert to json string-like object
     json_data = json.dumps(json_mrgd)
 
-
     return json_data
 
 
 ################################################################################
-################################################################################
 ################# Interactive Map ##############################################
-################################################################################
 ################################################################################
 
 # df to help with selecting either max or min temp data
 # during plot creation and update callbacks
 plt_scheme_data = [('TMAX', 0, 130, '0,0', 'Average Maximum Temperature'),
                ('TMIN', 0, 130, '0,0', 'Average Minimum Temperature')]
-
 
 plt_scheme_df = pd.DataFrame(plt_scheme_data, columns=['field', 'min_range', 'max_range',
                                                'format', 'text'])
@@ -102,10 +87,10 @@ mnth_dict = {b:a for a, b in enumerate(month_abbr[1::], start=1)}
 def update_plot(attr, old, new):
     # the input yr is the year selected from the slider_yr
     yr = slider_yr.value
-    # mnth = slider_mnth.value
     mnth_txt = select_mnth.value
     mnth = mnth_dict[mnth_txt]
-    
+
+    # generate geojsondata for a given year and month
     new_data = json_wthr_data(yr, mnth)
 
     # the input tmp_dtype is either TMAX or TMIN input from select_temp box
@@ -117,8 +102,10 @@ def update_plot(attr, old, new):
 
     # update layout, clear old document, create new document
     layout = column(p, Column(select_temp), Column(slider_yr), Column(select_mnth))
-    curdoc().clear()
-    curdoc().add_root(layout)
+    
+    # make use of wrapper column to add and remove layouts via
+    # wrapper children attribute (children att is a glorified list)
+    C.children = [layout]
 
     # new data for updated plot
     geosource.geojson = new_data
@@ -135,19 +122,18 @@ def make_plot(field_name):
     # LinearColorMapper: num_range --> color sequence
     color_mapper = LinearColorMapper(palette=palette, low=min_range, high=max_range)
 
-
     # colorbar to help illustrate temperature
     format_tick = NumeralTickFormatter(format=field_format)
     color_bar = ColorBar(color_mapper=color_mapper, label_standoff=18,
                          formatter=format_tick, border_line_color=None,
-                         major_label_text_color='#fefefe',
+                         major_label_text_color='grey',
                          location=(0, 0))
 
     # figure object
     text = plt_scheme_df.loc[plt_scheme_df['field'] == field_name, 'text'].iloc[0]
 
     p = figure(title = text + ' by California County - 2016 to 2020 (°F)',
-               plot_height=650, plot_width=850,
+               height=650, width=850,
                toolbar_location=None)
 
     p.xgrid.grid_line_color = None
@@ -171,7 +157,6 @@ def make_plot(field_name):
 geosource = GeoJSONDataSource(geojson = json_wthr_data(2020, 1))
 input_field = 'TMAX'
 
-
 # color palette definition
 palette = brewer['RdYlBu'][10]
 
@@ -180,6 +165,9 @@ palette = brewer['RdYlBu'][10]
 hover = HoverTool(tooltips = [ ('County', '@county'),
                                ('Maximum Temperature', '@TMAX'),
                                ('Minimum Temperature', '@TMIN')])
+
+# top-level layout container
+C = column()
 
 # Call plotting function to generate figure
 p = make_plot(input_field)
@@ -193,15 +181,16 @@ select_temp = Select(title = 'Temp Max/Min', value='Average Maximum Temperature'
                 options=['Average Maximum Temperature', 'Average Minimum Temperature'])
 select_temp.on_change('value', update_plot)
 
-
 # select_mnth: Select object to select views based on month
 select_mnth = Select(title="Month", value='Jan', options=list(month_abbr[1::]))
 select_mnth.on_change('value', update_plot)
 
-
-# make a column layout of Column(widget_name) and plot, and add it to the current document
-# display the current document
+# make a column layout of plot and Column(widget_name) elements
 layout = column(p, Column(select_temp), Column(slider_yr), Column(select_mnth))
 
+# add this layout to the top-level container
+C.children = [layout]
+
+# display top-level container to the current document
 curdoc().theme = Theme(filename=theme_pth)
-curdoc().add_root(layout)
+curdoc().add_root(C)
